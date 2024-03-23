b272728dff5ce3bc3ce55920f1dd3598ebb606b8
Abuthakir <abudx.at@gmail.com>
2024-03-23 22:36:21 +0530

    code corrections

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { sendFeedBack, getFeedBack, deleteFeedbacks } from "../controllers/feedBackControllers.mjs";
+import { sendFeedBack, getFeedBack, deleteFeedbacks } from "../controllers/feedBackControllers.js";
 const router = express.Router();
 router.get("/", getFeedBack);
 router.post("/", sendFeedBack);
