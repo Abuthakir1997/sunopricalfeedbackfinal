082ccecc5ef2551c0d6a507f0443584090fbdf58
Abuthakir <abudx.at@gmail.com>
2024-03-24 18:02:31 +0530

    code corrections

--- backend/server.js
+++ backend/server.js
@@ -7,24 +7,25 @@ import path from "path";
 import { fileURLToPath } from 'url';
 import feedbackRoutes from "../backend/routes/feedbackRoutes.js";
 dotenv.config();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 import connectDB from "./config/db.mjs";
 const app = express();
 connectDB();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
-if (process.env.NODE_ENV === "production") {
-    //set static folder
-    app.use(express.static(path.join(__dirname, "../../frontend/build")));
-    // any route that is not api will be redirected ti index.html
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../../frontend/build", "index.html")))
-}
-else {
-    app.get("/", (req, res) => {
-        res.send("Api is running...");
+
+app.use("/api/feedback", feedbackRoutes);
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....')
     })
 }
-app.use("/api/feedback", feedbackRoutes);
+
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
